618f7d33070b6f114bd5e223d17c26cf92e07a7c
developerraushan <developerraushan@gmail.com>
2022-01-06 21:23:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+const api = {
+  key: "2cb57ef648298f416758e2910b88d3c0",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 const App = () => {
   return (
     <div className="text-3xl text-center text-red-300">
